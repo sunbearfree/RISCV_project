efbcf7378de074a5507fe5003228d67a188ebe33
eKristensen <emil@ekx.dk>
2017-11-27 22:46:09 +0100

    load og jalr fix

--- src/main/scala/processor_main.scala
+++ src/main/scala/processor_main.scala
@@ -5,8 +5,12 @@ object processor_main {
   var pc: Int = 0                                                    //Program counter
   var programLoopBreak: Int = 0                                      //Break variable for program loading loop
   var loopBreak: Int = 0                                             //Break variable for main program loop
+  var computenumber: Int = 1
 
   var reg: Array[Int] = Array.fill[Int](32)(0)                       //Register array
+
+  reg(1) = 0x7ffffff0 // Initial state in venus for some reason
+  reg(2) = 0x10000000
 
   var mem: HashMap[Int,Int] = new HashMap[Int,Int]()                 //Memory array - kunne det klares med en Byte????
 
@@ -17,9 +21,12 @@ object processor_main {
     println("Hello RISC-V World!\n")
 
     // Load bin file
-    val programByteArray = Files.readAllBytes(Paths.get("addlarge.bin"))
+    val programByteArray = Files.readAllBytes(Paths.get("loop.bin"))
     while (programLoopBreak < programByteArray.length) {
-      val byteStr: Int = ((programByteArray(programLoopBreak + 3 & 0xff) << 24) + ((programByteArray(programLoopBreak + 2) & 0xff) << 16) + ((programByteArray(programLoopBreak + 1) & 0xff) << 8) + ((programByteArray(programLoopBreak)) & 0xff))
+      val byteStr: Int =  ((programByteArray(programLoopBreak + 3) & 0xff) << 24) +
+                          ((programByteArray(programLoopBreak + 2) & 0xff) << 16) +
+                          ((programByteArray(programLoopBreak + 1) & 0xff) << 8)  +
+                          (programByteArray(programLoopBreak) & 0xff)
       progr(pc) = byteStr
       pc = pc + 4                                                   //Save instructions in a 32bit integer
       programLoopBreak = programLoopBreak + 4
@@ -27,7 +34,21 @@ object processor_main {
     val programLength: Int = pc                                     //Save program length and set counter to 0 again
     pc = 0
 
+    println("Program length: "+programLength/4)
+
+    //for (i <- 0 to programLength/4-1) {
+    //  (1 to (8-progr(i*4).toHexString.length())).foreach(_ => print("0"))
+    //  println(progr(i*4).toHexString)
+    //}
+
     while (loopBreak == 0) {                                        //Program loop
+
+      print("%02d".format(computenumber)+": ")
+      computenumber = computenumber + 1
+      (1 to (8-progr(pc).toHexString.length())).foreach(_ => print("0"))
+      print(progr(pc).toHexString)
+      print(" pc: "+pc/4)
+      println()
 
       val instr: Int = progr(pc)                                    //Load instruction from program array
       val opcode: Int = instr & 0x7f
@@ -38,7 +59,9 @@ object processor_main {
       val rs2: Int = (instr >> 20) & 0x1f
 
       var imm_I: Int = (instr >> 20) & 0xfff
+      //println("imm_I just loaded: "+imm_I.toHexString);
       if((imm_I>>11)==1) imm_I = imm_I ^ 0xfffff000                 // Check if negative
+      //println("imm_I after neg check: "+imm_I.toHexString);
 
       var imm_S: Int = ((((instr >> 25) & 0x7f) << 5) + ((instr >> 7) & 0x1F))& 0xfff
       if((imm_S>>11)==1) imm_S = imm_S ^ 0xfffff000                 // Check if negative
@@ -109,13 +132,21 @@ object processor_main {
         case 0x23 => func3 match {                                              // Store instructions
           case 0x0 => mem(reg(rs1) + imm_S) = reg(rs2) & 0xFF       //SB
           case 0x1 => mem(reg(rs1) + imm_S) = reg(rs2) & 0xFFFF     //SH
-          case 0x2 => mem(reg(rs1) + imm_S) = reg(rs2)              //SW
+          case 0x2 => {
+            mem(reg(rs1) + imm_S) = reg(rs2)              //SW
+            //println("SW debug, addr: "+ (reg(rs1) + imm_S))
+          }
           case _ => println("ERROR3_1" + func3)
         }
         case 0x3 => func3 match {                                               //Load instructions
           case 0x0 => reg(rd) = mem.getOrElse(reg(rs1) + imm_I, 0) & 0XFF        //LB
           case 0x1 => reg(rd) = mem.getOrElse(reg(rs1) + imm_I, 0) & 0XFFFF      //LH
-          case 0x2 => reg(rd) = mem.getOrElse(reg(rs1) + imm_I, 0)               //LW
+          case 0x2 => {
+            reg(rd) = mem.getOrElse(reg(rs1) + imm_I, 0)               //LW
+            //println("LW reg del, addr: "+ reg(rs1))
+            //println("LW, imm_I del, addr: "+imm_I)
+            //println("LW debug, addr: "+ (reg(rs1) + imm_I))
+          }
           case 0x4 => reg(rd) = mem.getOrElse(reg(rs1) + imm_I, 0) & 0XFF        //LBU -- KORREKT????
           case 0x5 => reg(rd) = mem.getOrElse(reg(rs1) + imm_I, 0) & 0XFFFF      //LHU -- KORREKT????
           case _ => println("ERROR3_1" + func3)
@@ -144,7 +175,7 @@ object processor_main {
         case 0x67 => func3 match {
           case 0x0 =>                                               //JALR     -- Korrekt?????
             reg(rd) = pc + 4
-            pc = pc + (imm_I+reg(rs1)) - 4
+            pc = (reg(rs1)+imm_I) - 4
           case _ => println("ERROR3_1" + func3)
         }
         case 0x6f =>                                                //JAL
@@ -180,9 +211,14 @@ object processor_main {
 
       reg(0) = 0
       pc = pc + 4                                                   //Add to program counter
-      //for (i <- reg.indices) print(reg(i) + " ")                    //used for debugging
+
+      for (i <- reg.indices) print(reg(i) + " ")                    //used for debugging
+      println()
+      //println("Memory")
+      //for ((k,v) <- mem) printf("key: %s, value: %s\n", k, v)
       //println()
       if (pc >= programLength) {
+        println("ended because pc was: "+pc/4)
         loopBreak = 1
       }
     }
@@ -191,7 +227,7 @@ object processor_main {
     println("\n")
 
     println("Register overview (hex):")
-    for (i <- reg.indices) {
+    for (i <- 10 to 17) {
       print("%02d".format(i)+": ")
       (1 to (8-reg(i).toHexString.length())).foreach(_ => print("0"))
       print(reg(i).toHexString + "\n")
@@ -199,6 +235,28 @@ object processor_main {
     println("\n")
     println("reg.length: "+reg.length)
     println("\n")
+/*
+    println("Res file registers");
+
+    var bc: Int = 0
+    var resLoopBreak: Int = 0
+    val resByteArray = Files.readAllBytes(Paths.get("addlarge.res"))
+    var res: HashMap[Int,Int] = new HashMap[Int,Int]()               //program array
+    while (resLoopBreak < resByteArray.length) {
+      val byteStr: Int = ((resByteArray(programLoopBreak + 3 & 0xff) << 24) + ((resByteArray(programLoopBreak + 2) & 0xff) << 16) + ((resByteArray(programLoopBreak + 1) & 0xff) << 8) + ((resByteArray(programLoopBreak)) & 0xff))
+      res(bc) = byteStr
+      bc = bc + 4                                                   //Save instructions in a 32bit integer
+      resLoopBreak = resLoopBreak + 4
+    }
+
+    println("Res overview (hex):")
+    for (i <- 0 to resByteArray.length/4-4) {
+      print("%02d".format(i)+": ")
+      (1 to (8-res(i*4).toHexString.length())).foreach(_ => print("0"))
+      print(res(i*4).toHexString + "\n")
+    }
+    println("\n")
+*/
     println("Program exit")
   }
 }
